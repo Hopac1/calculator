4938666bfc2ad24a92310ed45d89ede8f7f96c4a
Hopac1 <chris1101@hotmail.co.uk>
2021-10-01 16:38:16 +0100

    let calculator complete basic two-number arithmetic

--- script.js
+++ script.js
@@ -1,4 +1,68 @@
-let sum = 0;
+const numberTextBox = document.querySelector(".numbers-entry");
+const buttons = document.querySelectorAll(".button");
+const numberButtons = document.querySelectorAll(".numbers");
+const clearButton = document.querySelector(".clear");
+const clearEntryButton = document.querySelector(".clear-entry");
+const operatorButtons = document.querySelectorAll(".operator");
+const equalButton = document.querySelector(".equal");
+
+let total = 0;
+let operator;
+let firstNumber;
+// numberTextBox.value = firstNumber;
+
+let nextNumber;
+
+
+// Event Listeners
+numberButtons.forEach(button => button.addEventListener("click", () => {
+    console.log(button.textContent);
+    if (firstNumber) {
+        changeSecondNumDisplay(button);
+    } else {
+        changeFirstNumDisplay(button);
+    }
+}));
+
+operatorButtons.forEach(opButton => opButton.addEventListener("click", () => {
+    storeOperator(opButton);
+}))
+
+equalButton.addEventListener("click", () => {
+    if (firstNumber && nextNumber) {
+        operate(operator, firstNumber, nextNumber);
+    }
+})
+
+clearButton.addEventListener("click", clearNumbers);
+
+
+// Functions
+function clearNumbers() {
+    numberTextBox.value = "";
+    firstNumber = null;
+    nextNumber = null;
+    total = 0;
+}
+
+function changeFirstNumDisplay(button) {
+    firstNumber = parseInt(button.textContent);
+    numberTextBox.value += firstNumber;
+}
+
+function changeSecondNumDisplay(button) {
+    nextNumber = parseInt(button.textContent);
+    numberTextBox.value += nextNumber;
+}
+
+function storeOperator(buttonPressed) {
+    operator = buttonPressed.textContent;
+    numberTextBox.value = `${firstNumber} ${operator} `
+}
+
+function showSum() {
+    numberTextBox.value = `${firstNumber} ${operator} ${nextNumber} = ${total}`
+}
 
 function operate(operatorSymbol, firstNum, secondNum) {
     if (operatorSymbol === "+") {
@@ -9,18 +73,19 @@ function operate(operatorSymbol, firstNum, secondNum) {
         divide(firstNum, secondNum)
     } else if (operatorSymbol === "*") {
         multiply(firstNum, secondNum)
-    }
+    } 
+    showSum();
 }
 
 function add(numOne, numTwo) {
-    sum = numOne + numTwo;
-    console.log(sum);
+    total = numOne + numTwo;
+    console.log(total);
     
 }
 
 function subtract(numOne, numTwo) {
-    sum = numOne - numTwo;
-    console.log(sum);
+    total = numOne - numTwo;
+    console.log(total);
 }
 
 function divide(numOne, numTwo) {
@@ -28,13 +93,13 @@ function divide(numOne, numTwo) {
         console.log("Cannot divide by zero");
         return;
     }
-    sum = numOne / numTwo;
-    console.log(sum);
+    total = numOne / numTwo;
+    console.log(total);
     
 }
 
 function multiply(numOne, numTwo) {
-    sum = numOne * numTwo;
-    console.log(sum);
+    total = numOne * numTwo;
+    console.log(total);
 }
 
